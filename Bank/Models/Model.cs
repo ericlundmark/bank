a1c08ae5fe0f7b161bf189c7396a61d559c500a0
ericlundmark <erilu506@gmail.com>
2015-08-30 16:52:48 +0200

    added model class

---
+++ Bank/Models/Model.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bank.Models
+{
+    public class Model
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTimeOffset Created { get; set; }
+
+        public Model()
+        {
+            Created = DateTime.UtcNow;
+        }
+    }
+}
--- Bank/Models/Wager.cs
+++ Bank/Models/Wager.cs
@@ -1,13 +1,7 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Bank.Models
+﻿namespace Bank.Models
 {
     public class Wager : Model
     {
-        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-
         public double Amount { get; set; }
         public double Odds { get; set; }
         public Competition Competition { get; set; }
