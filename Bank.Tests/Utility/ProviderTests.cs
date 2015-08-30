8a32dd5e3b75612bf726b79baf819bca96ce9942
ericlundmark <erilu506@gmail.com>
2015-08-30 19:00:43 +0200

    Added provider test

---
+++ Bank.Tests/Utility/ProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Bank.Models;
+using Bank.Repositories;
+using Bank.Utility;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Provider = Bank.Utility.Provider;
+
+namespace Bank.Tests.Utility
+{
+    [TestClass]
+    public class ProviderTests
+    {
+        [TestMethod]
+        public void Run()
+        {
+            var mockCollector = new Mock<IDataCollector>();
+            mockCollector.Setup(c => c.CollectData()).Returns("data");
+
+            var mockParser = new Mock<ICompetitionParser>();
+            mockParser.Setup(p => p.Parse("data")).Returns(new List<Competition>());
+
+            var mockRepository = new Mock<ICompetitionRepository>();
+
+            var provider = new Provider()
+            {
+                Collector = mockCollector.Object,
+                Competitions = mockRepository.Object,
+                Parser = mockParser.Object
+            };
+            provider.Run();
+
+            mockRepository.Verify(r => r.Add(new List<Competition>()));
+        }
+    }
+}
